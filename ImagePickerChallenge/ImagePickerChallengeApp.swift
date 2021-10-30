f7f3f32ad8dbc5d3d82d5f8efeadb72d91e2ea8e
ALeX <alexdeoliveira.poli@gmail.com>
2021-10-30 09:06:14 -0300

    Initial Commit

---
+++ ImagePickerChallenge/ImagePickerChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePickerChallengeApp.swift
+//  ImagePickerChallenge
+//
+//  Created by Alex Oliveira on 30/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ImagePickerChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
